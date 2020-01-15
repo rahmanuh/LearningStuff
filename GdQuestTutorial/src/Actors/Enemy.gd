12de1aac2de31afbd9fc587bb4b1ae8dbd70b664
Rahmanu Hermawan <rahmanu.hermawan@gmail.com>
2020-01-15 15:44:14 +0700

    min 1:35. enemy still not vanish if get stomped

--- GdQuestTutorial/src/Actors/Enemy.gd
+++ GdQuestTutorial/src/Actors/Enemy.gd
@@ -1,7 +1,15 @@
 extends "res://src/Actors/Actor.gd"
 
 func _ready() -> void:
+	set_physics_process(false)
 	_velocity.x = -_speed.x
+	
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	queue_free()
+#	get_node("CollisionShape2D").disabled  = true
+	
 	
 func _physics_process(delta: float) -> void:
 	_velocity.y += _gravity * delta
