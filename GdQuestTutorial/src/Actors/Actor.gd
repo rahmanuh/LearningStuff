b1160404297cf52853e0116d931aad5eef59ef62
Rahmanu Hermawan <rahmanu.hermawan@gmail.com>
2020-01-15 14:19:39 +0700

    just first commit

---
+++ GdQuestTutorial/src/Actors/Actor.gd
@@ -0,0 +1,7 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP
+export var _speed: = Vector2(300.0, 1000.0)
+export var _gravity: = 4000.0
+var _velocity: = Vector2.ZERO
